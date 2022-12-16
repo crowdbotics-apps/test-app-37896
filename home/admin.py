6b3ba1c5fe2585b773217209e378043d3a6aea32
crowdbotics-apps <team@crowdbotics.com>
2022-12-16 18:54:53 +0000

    Update models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Base_Table
+admin.site.register(Base_Table)
 
 # Register your models here.
--- home/models.py
+++ home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+class Base_Table(models.Model):
+    'Generated Model'
+    oid = models.BigIntegerField()
+    description = models.TextField(null=True,blank=True,)
+    dateChanged = models.DateTimeField(null=True,blank=True,)
